6fc1c727a4014737806cb526337b01d778be64ac
AlejandroBR10 <alejabaldera@gmail.com>
2025-03-06 10:21:49 -0600

    Auth for routes

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -3,9 +3,10 @@ import { applyDecorators, SetMetadata, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../guards/auth.guards';
 import { RolesGuard } from '../guards/roles.guards';
 import { Roles } from './roles.decorators';
+import { ROLES } from '../constants/roles.constants';
 
-export const Auth = (...roles: string[])  => {
-    roles.push("Admin");
+export const Auth = (...roles: ROLES[])  => {
+    roles.push(ROLES.ADMIN);
   return applyDecorators(
     Roles(roles),
     UseGuards(AuthGuard, RolesGuard),
