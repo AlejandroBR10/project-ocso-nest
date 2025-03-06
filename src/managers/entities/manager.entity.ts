6a424840fe1db97698d4d6deedef938b64669fa3
AlejandroBR10 <alejabaldera@gmail.com>
2025-03-06 09:28:08 -0600

    User relations to managers and employees

--- src/managers/entities/manager.entity.ts
+++ src/managers/entities/manager.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import {Location} from "src/locations/entities/location.entity";
+import { User } from "src/auth/entities/user.entity";
 
 @Entity()
 export class Manager {
@@ -15,5 +16,10 @@ export class Manager {
     managerPhoneNumber : string;
     @OneToOne(() => Location)
     profile: Location
+    @OneToOne(() => User)
+    @JoinColumn({
+        name: 'userId'
+    })
+    user : User;
 
 }
